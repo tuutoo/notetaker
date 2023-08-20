abee80a4286dee4ae009220f1327d017d4c815cf
tuutoo <tuutoo@iMac.com>
2023-08-21 00:12:27 +0800

    feat: 🎸 done

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
+import { topicRouter } from './routers/topic';
 import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { noteRouter } from './routers/note';
 
 /**
  * This is the primary router for your server.
@@ -8,6 +10,8 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  topic: topicRouter,
+  note: noteRouter,
 });
 
 // export type definition of API
